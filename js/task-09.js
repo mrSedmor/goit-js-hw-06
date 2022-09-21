f56133c8503fa6910d5b4fe8f1dc699b7c83f6b2
Serhei Honcharov <mrsedmor@gmail.com>
2022-09-21 21:38:15 +0200

    task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 0xffffff)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
+
+const body = document.body;
+const changeColorButton = document.querySelector('.change-color');
+const colorSpan = document.querySelector('.color');
+
+changeColorButton.addEventListener('click', () => {
+  const color = getRandomHexColor();
+
+  body.style.backgroundColor = color;
+  colorSpan.textContent = color;
+});
