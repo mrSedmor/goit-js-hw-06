7c6023228331723a6e4a045811ad10c67f51f1da
Serhei Honcharov <mrsedmor@gmail.com>
2022-09-21 17:56:20 +0200

    task 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,12 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
+const ingredientsList = document.querySelector('#ingredients');
+
+const ElementsList = ingredients.map(ingredientName => {
+  const element = document.createElement('li');
+  element.textContent = ingredientName;
+  element.classList.add('item');
+  return element;
+});
+
+ingredientsList.append(...ElementsList);
