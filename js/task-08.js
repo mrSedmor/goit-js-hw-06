56127138d5ade80ccb380030c67fa449d9e4d6e1
Serhei Honcharov <mrsedmor@gmail.com>
2022-09-21 21:38:08 +0200

    task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,27 @@
+const loginForm = document.querySelector('.login-form');
 
+function onSubmit(event) {
+  event.preventDefault();
+
+  const form = event.currentTarget;
+  const elements = form.elements;
+
+  const data = {
+    email: elements.email.value.trim().toLowerCase(),
+    password: elements.password.value,
+  };
+
+  for (const [key, value] of Object.entries(data)) {
+    if (value !== '') continue;
+
+    elements[key].focus();
+    alert('Все поля должны быть заполнены!');
+
+    return;
+  }
+
+  console.log(data);
+  form.reset();
+}
+
+loginForm.addEventListener('submit', onSubmit);
