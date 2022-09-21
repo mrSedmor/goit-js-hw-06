8b93d62294eccc3e0d6b18350528d7d571eed028
Serhei Honcharov <mrsedmor@gmail.com>
2022-09-21 17:56:34 +0200

    task 3

--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,9 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+const markup = images
+  .map(({ url, alt }) => `<li class="gallery__item"><img src="${url}" alt="${alt}" /></li>`)
+  .join('');
+
+document.querySelector('.gallery').insertAdjacentHTML('afterbegin', markup);
